5be06d5159f5da80dde5a49b91ea96d993a21d03
Najmus Sakib <106301767+itsnajmus@users.noreply.github.com>
2023-03-27 14:46:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,34 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+function fetchApiData(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+}
 
+function fetchDataWithPromiseAll() {
+  const startTime = performance.now();
+  Promise.all(apiUrls.map(fetchApiData))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = endTime - startTime;
+      document.getElementById("output-all").innerHTML = `Promise.all took ${timeTaken} milliseconds`;
+      console.log(data);
+    });
+}
+
+function fetchDataWithPromiseAny() {
+  const startTime = performance.now();
+  Promise.any(apiUrls.map(fetchApiData))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = endTime - startTime;
+      document.getElementById("output-any").innerHTML = `Promise.any took ${timeTaken} milliseconds`;
+      console.log(data);
+    });
+}
+
+fetchDataWithPromiseAll();
+fetchDataWithPromiseAny();
 // You can write your code here
